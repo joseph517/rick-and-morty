661096c43df122bea787724ce20a8f465dd12462
joseph517 <alvarovergara1993@hotmail.com>
2022-10-24 12:21:10 -0500

    paginacion de episodeos

--- src/pages/EpisodesPages.js
+++ src/pages/EpisodesPages.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import Episodes from '../component/Episodes'
+import Pagination from 'react-js-pagination'
 
 const EpisodesPages = () => {
-
-    const [episodes, setEpisodes] = useState(null)
+    const [episodes, setEpisodes] = useState([])
+    const [numberPage, setNumberPage] = useState(1);
+    const [pageConfig, setPageConfig] = useState({});
 
     useEffect(() => {
         reqApi()
-    }, [])
+    }, [numberPage])
 
     const reqApi = async () => {
-        const api = await fetch('https://rickandmortyapi.com/api/episode')
+        const api = await fetch(`https://rickandmortyapi.com/api/episode?page=${numberPage}`)
         const episodeApi = await api.json()
+        let itemPages = Math.round(episodeApi.info.count / episodeApi.info.pages)
+        setPageConfig({ ...episodeApi.info, itemPages })
         setEpisodes(episodeApi.results)
     }
-    console.log(episodes)
 
+    function handlePageChange(pageNumber) {
+        setNumberPage(pageNumber);
+    }
 
 
     return (
@@ -27,6 +33,18 @@ const EpisodesPages = () => {
                         <Episodes episodes={episodes} />
                     </div>
                 </>}
+            <div>
+                <Pagination
+                    activePage={numberPage}
+                    itemsCountPerPage={pageConfig.itemPages}
+                    totalItemsCount={pageConfig.count}
+                    pageRangeDisplayed={5}
+                    onChange={handlePageChange}
+                    itemClass="page-item"
+                    linkClass="page-link"
+                />
+
+            </div>
         </div>
     )
 }
